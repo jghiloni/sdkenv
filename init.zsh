ed6134c06f46a80318655648a3300f70582355e6
Josh Ghiloni <jghiloni>
2021-02-05 15:42:35 -0700

    boo java

--- init.zsh
+++ init.zsh
@@ -1,9 +1,7 @@
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
-export JAVA_HOME=$(/usr/libexec/java_home)
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$HOME/bin:$GOPATH/bin:$PATH"
-export PATH="$PATH:$HOME/.rvm/bin"
 
 eval "$(direnv hook zsh)"

5a20dc7cef0a6c9ae3c425ea795a500f3605c937
Josh Ghiloni <ghiloni@gmail.com>
2021-02-07 12:05:30 -0700

    Add gpg stuff

--- init.zsh
+++ init.zsh
@@ -1,7 +1,7 @@
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
 
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$HOME/bin:$GOPATH/bin:$PATH"
+export GPG_TTY=$(tty)
 
 eval "$(direnv hook zsh)"
